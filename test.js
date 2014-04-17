c0a40168714a4fb2331b5a295bc7d144e687726f
CodeDestroyer <cunninghamx@gmail.com>
2014-04-17 17:38:53 -0400

    speed base scroll
    speed bass scroll started with stop and increase speed.

--- test.js
+++ test.js
@@ -12,23 +12,44 @@
 
         this._defaults = defaults;
         this._name = pluginName;
-        this.position = $(window).scrollTop();
+        this.storyHeight = $('#para').height();
+        this.timeToScroll = this.calculateTime(this.storyHeight);
         this.init();
     }
 
     Plugin.prototype = {
 
         init: function() {
-
+            $(this.element).animate({
+                scrollTop: this.storyHeight
+            },this.timeToScroll);
+        },
+        //TODO : Make a function to base speed off something like difficulty
+        calculateTime : function(height){
+            return height * 50;
+        },
+        stopAnimation: function(){
+            $(this.element).stop();
+            console.log("test");
+        },
+        //Totally wrong.. we need to measure how much we have scrolled before we reassign a speed. because its going to redo the gap over the same time.
+        increaseSpeed: function(){
+            this.timeToScroll = this.timeToScroll - 10000;
+            $(this.element).stop();
+            this.init();
+            console.log(this.timeToScroll);
         }
+
     };
-    $.fn[pluginName] = function ( options ) {
-        return this.each(function () {
-            if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName,
-                    new Plugin( this, options ));
+    $.fn[pluginName] = function(options) {
+        return this.each(function() {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+            }
+            else if ($.isFunction(Plugin.prototype[options])) {
+                $.data(this, 'plugin_' + pluginName)[options]();
             }
         });
-    };
+    }
 
 })( jQuery, window, document );
